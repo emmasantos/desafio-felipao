f30d492ba9dd2ba857e8afb210f0d8948310e2df
Emanuelly Santos <moreiransantos@gmail.com>
2024-01-15 02:15:17 -0300

    update classificador de nível de herói

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+//variáveis nome do jogador, quantidade de xp, nível de jogador
+let nomeJogador = "Eden333";
+let quantidadeXP = 4444;
 
-console.log("oi");
+//tomada de decisão, classifica nível de jogador de acordo com a quantidade de XP
+if(quantidadeXP <= 1000){
+    nivelJogador = "Ferro"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 1001) && (quantidadeXP < 2000)){
+    nivelJogador = "Bronze"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 2001) && (quantidadeXP < 5000)){
+    nivelJogador = "Prata"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 5001) && (quantidadeXP < 7000)){
+    nivelJogador = "Ouro"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 7001) && (quantidadeXP < 8000)){
+    nivelJogador = "Platina"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 8001) && (quantidadeXP < 9000)){
+    nivelJogador = "Ascendente"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if ((quantidadeXP >= 9001) && (quantidadeXP < 10000)){
+    nivelJogador = "Imortal"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}else if (quantidadeXP >= 10001){
+    nivelJogador = "Radiante"
+    console.log('O Herói de nome ' + nomeJogador + " está no nível " + nivelJogador + "!")
+}
